f64cf6dc3777029e0f54763ad5b55d1c0a3183dc
Tim Hoang <tim@jixie.io>
2023-10-08 22:59:35 +0700

    Remove redundant

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -1,5 +1,5 @@
 ##!/bin/bash
-# npm run build &&
+npm run build &&
 ssh -p $SSH_PROD_SERVER_PORT root@$PROD_SERVER_IP  'rm -rf /var/www/html/tienhoangdevblog' &&
 scp -P $SSH_PROD_SERVER_PORT -r build root@$PROD_SERVER_IP:/var/www/html/tienhoangdevblog
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-{
+module.exports = {
   enforce: 'pre',
-    include: paths.appSrc,
-      test: /\.(js|jsx|mjs)$/,
-        use: [{
-          loader: require.resolve('eslint-loader'),
-          options: {
-            formatter: eslintFormatter,
-            eslintPath: require.resolve('eslint'),
-            emitWarning: true, 👈 HERE
-          },
-        }],
-},
+  include: paths.appSrc,
+  test: /\.(js|jsx|mjs)$/,
+  use: [{
+    loader: require.resolve('eslint-loader'),
+    options: {
+      formatter: eslintFormatter,
+      eslintPath: require.resolve('eslint'),
+      emitWarning: true,
+    },
+  }],
+}
